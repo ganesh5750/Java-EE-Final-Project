63ee4cc2b0dc394ae32b3bd7be6f4dd9680de509
Ganesh <ganesh.patel11@gmail.com>
2015-08-04 12:20:42 -0400

    style sheet uploading

--- src/main/java/com/oracle/damageProperty.java
+++ src/main/java/com/oracle/damageProperty.java
@@ -23,6 +23,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,6 +32,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author Ganesh
  */
+@WebServlet (name = "damageProperty", urlPatterns= {"/damageProperty"})
 public class damageProperty extends HttpServlet {
 
     /**
